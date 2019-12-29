8525da3e34ecf56b4e1ebf2738ae57585b8893af
root <heshamxa@gmail.com>
2019-12-29 23:34:43 +0000

    add details to thestring

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ var commentRoutes    = require("./routes/comments"),
 	indexRoutes      = require("./routes/index");
 
 //mongoose.connect("mongodb://localhost:27017/yelp_camp" ,{ useNewUrlParser: true });
-mongoose.connect('mongodb+srv://heshamxa:kamigoinazawa@cluster0-uanoq.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://heshamxa:kamigoinazawa@cluster0-uanoq.mongodb.net/yelp_camp?retryWrites=true&w=majority', {
 	useNewUrlParser: true,
 	useCreateIndex: true
 }).then(() => {
 	console.log('Connected to DB!'); 
-}).catch(err => {
+}).catch(err => { 
 	console.log('ERROR:', err.message);
 });
 mongoose.set('useNewUrlParser', true);
