1deb3d5ec301bc72fbde5684b177ba430972d8e4
root <heshamxa@gmail.com>
2020-01-04 23:58:29 +0000

    use mongo url

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ var commentRoutes    = require("./routes/comments"),
 	campgroundRoutes = require("./routes/campgrounds"),
 	indexRoutes      = require("./routes/index"); 
 //console.log(process.env.DATABASEURL);
-//mongoose.connect("mongodb://localhost:27017/yelp_camp" ,{ useNewUrlParser: true });
-mongoose.connect("mongodb+srv://heshamxa:neemleen@cluster0-uanoq.mongodb.net/test?retryWrites=true&w=majority", {
+mongoose.connect("mongodb://localhost:27017/yelp_camp" ,{ useNewUrlParser: true });
+//mongoose.connect("mongodb+srv://heshamxa:neemleen@cluster0-uanoq.mongodb.net/test?retryWrites=true&w=majority", {
 	useNewUrlParser: true,
 	useCreateIndex: true
  }).then(() => {
